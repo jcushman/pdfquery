cffc07d954c4e8bf32264dfdd6e5b91a880f8b4f
Jack Cushman <jcushman@gmail.com>
2012-04-16 16:39:19 -0400

    updated dist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pdfquery',
-    version='0.1.2',
+    version='0.1.3',
     author=u'Jack Cushman',
     author_email='jcushman@gmail.com',
     packages=find_packages(),
