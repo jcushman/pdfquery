0a4156eb4e3151b4e6afd83a78c418a21e79586a
Henrik Tudborg <henrik@tudb.org>
2013-08-16 12:55:43 +0200

    adding support for non-string argument list (seems more css-line)

--- pdfquery/pdftranslator.py
+++ pdfquery/pdftranslator.py
@@ -9,7 +9,10 @@ from cssselect import xpath as cssselect_xpath
 class PDFQueryTranslator(cssselect_xpath.GenericTranslator):
 
     def xpath_in_bbox_function(self, xpath, fn):
-        x0,y0,x1,y1 = map(float, fn.arguments[0].value.split(","))
+        if len(fn.arguments) > 1:
+            x0,y0,x1,y1 = [float(t.value) for t in fn.arguments]
+        else:
+            x0,y0,x1,y1 = map(float, fn.arguments[0].value.split(","))
         # TODO: seems to be doing < rather than <= ???
         xpath.add_condition("@x0 >= %s" % x0)
         xpath.add_condition("@y0 >= %s" % y0)
@@ -18,7 +21,10 @@ class PDFQueryTranslator(cssselect_xpath.GenericTranslator):
         return xpath
 
     def xpath_overlaps_bbox_function(self, xpath, fn):
-        x0,y0,x1,y1 = map(float, fn.arguments[0].value.split(","))
+        if len(fn.arguments) > 1:
+            x0,y0,x1,y1 = [float(t.value) for t in fn.arguments]
+        else:
+            x0,y0,x1,y1 = map(float, fn.arguments[0].value.split(","))
         # TODO: seems to be doing < rather than <= ???
         xpath.add_condition("@x0 <= %s" % x1)
         xpath.add_condition("@y0 <= %s" % y1)
