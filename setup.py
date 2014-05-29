cd0c9368ff2adad25273d717266def0715f7b59b
Jack Cushman <jcushman@gmail.com>
2014-05-29 18:31:37 -0400

    Increment version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pdfquery',
-    version='0.2.3',
+    version='0.2.4',
     author=u'Jack Cushman',
     author_email='jcushman@gmail.com',
     packages=find_packages(),
