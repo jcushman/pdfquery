2b77f704dfb5fd28721904bebf3e892c08c8a856
Jack Cushman <jcushman@gmail.com>
2015-12-21 20:42:34 -0500

    Version 0.4.1.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except ImportError:
 
 setup(
     name='pdfquery',
-    version='0.4.0',
+    version='0.4.1',
     author=u'Jack Cushman',
     author_email='jcushman@gmail.com',
     packages=find_packages(),
