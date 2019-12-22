7f83848e6ead157ecfb8563d592550b1fdda6020
Jack Cushman <jcushman@law.harvard.edu>
2019-12-22 17:05:29 -0500

    ignore index attributes in test xml comparison

--- tests/utils.py
+++ tests/utils.py
@@ -8,6 +8,9 @@ if sys.version_info[:2] < (2, 7):
 else:
     import unittest
 
+# ignore index= attribute in xml comparison, as it is not stable between python versions
+IGNORE_ATTRIBS = {'index'}
+
 class BaseTestCase(unittest.TestCase):
 
     def assertValidOutput(self, pdf, output_name):
@@ -38,10 +41,11 @@ class BaseTestCase(unittest.TestCase):
             output_path = "tests/%s_failed_output.xml" % output_name
             with open(output_path, "wb") as out:
                 out.write(tree_string)
+            # for debugging: run `pytest --lf --pdb` and then use etree.dump(e1), etree.dump(e2)
             e1, e2 = e.args[1:3]
             raise self.failureException("XML conversion of sample pdf has changed! Compare %s to %s" % (comparison_file, output_path)) from e
 
-    def xml_strings_equal(self, s1, s2):
+    def xml_strings_equal(self, s1, s2, ignore_attribs=IGNORE_ATTRIBS):
         """
             Return true if two xml strings are semantically equivalent (ignoring attribute ordering and whitespace).
         """
@@ -50,7 +54,7 @@ class BaseTestCase(unittest.TestCase):
             if e1.tag != e2.tag: raise self.failureException("Mismatched tags", e1, e2)
             if e1.text != e2.text: raise self.failureException("Mismatched text", e1, e2)
             if e1.tail != e2.tail: raise self.failureException("Mismatched tail", e1, e2)
-            if e1.attrib != e2.attrib: raise self.failureException("Mismatched attributes %s and %s" % (e1.attrib, e2.attrib), e1, e2)
+            if set(e1.attrib) - ignore_attribs != set(e2.attrib) - ignore_attribs: raise self.failureException("Mismatched attributes %s and %s" % (e1.attrib, e2.attrib), e1, e2)
             if len(e1) != len(e2): raise self.failureException("Mismatched children", e1, e2)
             for c1, c2 in zip(e1, e2):
                 elements_equal(c1, c2)
